53246b7c1adfd8fb7fb5bd6735d81048daff99c8
Serkan <mr.serkanbircan@gmail.com>
2026-02-21 18:08:28 +0300

    Add validations

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,6 +2,27 @@ import openapi from "@elysiajs/openapi";
 import { type } from "arktype";
 import { Elysia } from "elysia";
 
+const Payload = type({
+  username: "string.alphanumeric > 0",
+  password: "string",
+});
+
+const badRequestModule = type.module({
+  "#ValidationError": {
+    path: "string",
+    message: "string",
+  },
+  Schema: {
+    title: "string",
+    "detail?": "string",
+    status: "100 < number < 599",
+    instance: "string",
+    "errors?": "ValidationError[]",
+  },
+});
+
+type BadRequestSchema = typeof badRequestModule.Schema.infer;
+
 const app = new Elysia()
   .use(
     openapi({
@@ -20,7 +41,62 @@ const app = new Elysia()
       },
     }),
   )
+  .derive(({ request }) => ({
+    problem: ({
+      title = "Bad Request",
+      status: statusCode = 400,
+      detail,
+      errors,
+    }: Pick<
+      Partial<BadRequestSchema>,
+      "title" | "detail" | "status" | "errors"
+    > = {}) => {
+      return Response.json(
+        {
+          title,
+          detail,
+          status: statusCode,
+          instance: `${request.method} ${new URL(request.url).pathname}`,
+          errors,
+        } satisfies BadRequestSchema,
+        { status: statusCode },
+      );
+    },
+  }))
+  .onError(({ code, error, problem }) => {
+    if (code === "VALIDATION") {
+      return problem({
+        title: "Bad Request",
+        status: 400,
+        errors: error.all.map((e) => ({
+          path: e.path,
+          message: e.message,
+        })),
+      });
+    }
+    if (code === "INTERNAL_SERVER_ERROR" && !!problem) {
+      return problem({
+        title: "Internal Server Error",
+        status: 500,
+      });
+    }
+  })
   .get("/", () => "Hello Elysia")
+  .post(
+    "/sign-in",
+    ({ body }) => {
+      return {
+        status: 200,
+        id: "30PKTuqj4FstWTdFpQWS8PcxeHvZ1HFjxOUtAmriWGE=",
+      };
+    },
+    {
+      body: Payload,
+      response: {
+        400: badRequestModule.Schema,
+      },
+    },
+  )
   .listen(3000);
 
 console.log(
