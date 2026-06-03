8db2ba53672eac1acc75c7cadf806499790eb3a6
Serkan <mr.serkanbircan@gmail.com>
2026-06-03 13:47:04 +0300

    Fix build issue with bun

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(config => ({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+  resolve:
+    config.command === "build"
+      ? {
+          alias: {
+            "react-dom/server": "react-dom/server.node",
+          },
+        }
+      : undefined,
+}));
